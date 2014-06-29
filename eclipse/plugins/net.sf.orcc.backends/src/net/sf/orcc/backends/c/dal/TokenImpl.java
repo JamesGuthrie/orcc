3642517365b0dedff365158d3c13e4ea6dfc9de0
James Guthrie <james@prodigi.ch>
2014-06-30 01:50:00 +0200

    (DAL) Token: package out GetVars class

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/c/dal/TokenImpl.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/c/dal/TokenImpl.java
@@ -1,87 +1,21 @@
 package net.sf.orcc.backends.c.dal;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
 
-import net.sf.orcc.OrccRuntimeException;
-import net.sf.orcc.ir.Arg;
-import net.sf.orcc.ir.ArgByRef;
-import net.sf.orcc.ir.ArgByVal;
-import net.sf.orcc.ir.ExprBinary;
-import net.sf.orcc.ir.ExprInt;
-import net.sf.orcc.ir.ExprVar;
-import net.sf.orcc.ir.Expression;
-import net.sf.orcc.ir.InstCall;
-import net.sf.orcc.ir.InstLoad;
 import net.sf.orcc.ir.Instruction;
 import net.sf.orcc.ir.Var;
-import net.sf.orcc.ir.util.AbstractIrVisitor;
 
+
+/**
+ * Abstract implementation of Token functionality
+ *
+ * @author James Guthrie
+ *
+ */
 public abstract class TokenImpl implements Token, Comparable<Token> {
 
 	protected Instruction i;
-
-	private class GetVars extends AbstractIrVisitor<Collection<Var>> {
-
-		private Set<Var> vars;
-
-		public GetVars() {
-			super(true);
-			vars = new HashSet<Var>();
-		}
-
-		@Override
-		public Collection<Var> caseExpression(Expression expr) {
-			throw new OrccRuntimeException("Unhandled expression type: " + expr.getClass());
-		}
-
-		@Override
-		public Collection<Var> caseExprInt(ExprInt exprInt) {
-			return new HashSet<Var>();
-		}
-
-		@Override
-		public Collection<Var> caseExprBinary(ExprBinary exprBinary) {
-			vars.addAll(doSwitch(exprBinary.getE1()));
-			vars.addAll(doSwitch(exprBinary.getE2()));
-			return new HashSet<Var>(vars);
-		}
-
-		@Override
-		public Collection<Var> caseArgByVal(ArgByVal arg) {
-			return doSwitch(arg.getValue());
-		}
-
-		@Override
-		public Collection<Var> caseArgByRef(ArgByRef arg) {
-			vars.add(arg.getUse().getVariable());
-			return new HashSet<Var>(vars);
-		}
-
-		@Override
-		public Collection<Var> caseInstLoad(InstLoad instLoad) {
-			for (Expression e : instLoad.getIndexes()) {
-				vars.addAll(doSwitch(e));
-			}
-			return new HashSet<Var>(vars);
-		}
-
-		@Override
-		public Collection<Var> caseInstCall(InstCall instCall) {
-			for (Arg a : instCall.getArguments()) {
-				vars.addAll(doSwitch(a));
-			}
-			return new HashSet<Var>(vars);
-		}
-
-		@Override
-		public Collection<Var> caseExprVar(ExprVar exprVar) {
-			vars.add(exprVar.getUse().getVariable());
-			return new HashSet<Var>(vars);
-		}
-	}
 
 	@Override
 	public Collection<Var> dependencies() {
